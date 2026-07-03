2cb6803e5e0e213285777960fa18888ef0d2fe1b
Dorji Zangpo <dorjizangpo067@gmail.com>
2026-07-03 23:52:53 +0600

    add docs and comment

--- src/utils/jwt_setup.py
+++ src/utils/jwt_setup.py
@@ -13,6 +13,18 @@ def create_access_token(
     expiry: timedelta | None = None,
     refresh: bool | None = None,
 ) -> str:
+    """Generate New Token
+
+    Args:
+        user_data: dict[str, Any],
+        expiry: timedelta | None = None,
+        refresh: bool | None = None,
+
+    Returns:
+        token: str
+    """
+
+    # default value of expiry time
     if expiry is None:
         expiry = timedelta(seconds=config.JWT_EXPIRY)
 
@@ -31,6 +43,13 @@ def create_access_token(
 
 
 def decode_token(token: str) -> dict[str, Any] | None:
+    """Decode the Token
+    Args:
+        token: str
+
+    Returns:
+        token_data: dict[str, Any] or None if token is Invalid
+    """
     try:
         token_data = jwt.decode(
             jwt=token,
@@ -41,3 +60,17 @@ def decode_token(token: str) -> dict[str, Any] | None:
     except jwt.PyJWTError as e:
         logging.exception(e)
         return None
+
+
+def is_token_valid(token: str) -> bool:
+    """Verify token validity
+    Args:
+        token: str
+
+    Returns:
+        True if valid and False if Invalid token
+    """
+    token_data = decode_token(token=token)
+    if token_data is None:
+        return False
+    return True

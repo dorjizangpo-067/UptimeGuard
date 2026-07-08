a5ac26c60d254c9428131bb5aa72d7062b5a5147
Dorji Zangpo <dorjizangpo067@gmail.com>
2026-07-08 19:31:32 +0600

    remove username col form user table
    since it is small took using username and email feels like reduntand

--- src/models/model_auth.py
+++ src/models/model_auth.py
@@ -17,7 +17,6 @@ class User(Base):
     uid: Mapped[uuid.UUID] = mapped_column(
         pg.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
     )
-    username: Mapped[str] = mapped_column(String, unique=True, index=True)
     email: Mapped[str] = mapped_column(String, index=True, unique=True)
     full_name: Mapped[str] = mapped_column(String(30))
     password_hashed: Mapped[str] = mapped_column(String)
